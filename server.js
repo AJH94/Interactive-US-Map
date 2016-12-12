2c9856d95507fcffe1d030d03c77408d59aebee3
Khoi Doan <zombify.25@gmail.com>
2016-12-12 16:28:44 -0500

    Server now make requests with Google API
    (NOTE: Does not actually do anything with the data yet)

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ var http = require('http')
   , fs   = require('fs')
   , url  = require('url')
   , port = 8080
+  , request = require('request')
   , express = require('express')
   , app = express();  
 
 app.use(express.static('public'));
+
+app.get('/route', function (req, res) {
+    var beg = req.query.beg;
+    var end = req.query.end;
+    console.log("Incoming route: " + beg + " " + end);
+    request('https://maps.googleapis.com/maps/api/directions/json?origin=' + beg + '&destination=' + end + '&key=AIzaSyD_Bt9JAwFPencv-Vuy4UhtKk9oDRknO8g', function (error, response, body) {
+        routeInfo = JSON.parse(body);
+        console.log(routeInfo);
+        res.send(JSON.stringify(routeInfo));
+    })
+})
+
 
 app.listen(process.env.PORT || port, function () {
           console.log('Example app listening on port 8080!')
